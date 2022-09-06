9e000479c1c9e04a9694ca21a5e8071959d8af1d
attsumi <yattsumi@gmail.com>
2022-09-07 02:14:40 +0900

    fix typo seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,4 +1,4 @@
-mport React from "react"
+import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 import { useLocation } from "@reach/router"
