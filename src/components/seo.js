74b1d17d2b6d3168db5db487c9f19ddc4f64fce5
attsumi <yattsumi@gmail.com>
2022-09-08 11:35:12 +0900

    fix typo

--- src/components/seo.js
+++ src/components/seo.js
@@ -43,7 +43,7 @@ function Seo({ description, title, children }) {
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
-      <meta name="twitter:descripn" content={ogimage} />
+      <meta name="twitter:image" content={ogimage} />
       {children}
     </>
   )
