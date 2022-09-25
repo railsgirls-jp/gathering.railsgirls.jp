cd0b129a42baa277a1541c0aacce4940f964b9c2
attsumi <yattsumi@gmail.com>
2022-09-25 10:03:20 +0900

    keynote speaker と Special（仮）を追加した

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 import { Helmet } from "react-helmet"
-import { useBreakpoint } from 'gatsby-plugin-breakpoints'
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 import { graphql } from "gatsby"
 const IndexPage = () => (
   <Layout>
@@ -23,17 +23,47 @@ const IndexPage = () => (
         alt=""
         style={{ marginBottom: `var(--space-3)` }}
       />
-      <h1>
-      Rails Girls <br />
-      Gathering <br />
-      Japan 2022
-      </h1>
-      <p className={styles.intro}>
-        <b>2022/12/03 (Sat) <br/>
-        Online</b>{" "}
-      </p>
-      <a href="https://railsgirls.jp/code-of-conduct" target="_blank">Code of Conduct</a>
+      <div className={styles.textCenter}>
+        <div className={styles.title}>
+          <h1>
+          Rails Girls <br />
+          Gathering <br />
+          Japan 2022
+          </h1>
+        </div>
+        <div className={styles.date}>
+          <p className={styles.intro}>
+            2022/12/03 (Sat) <br/>
+            Online{" "}
+          </p>
+        </div>
+        <div className={styles.textCenter}>
+          <a href="https://railsgirls.jp/code-of-conduct" target="_blank">Code of Conduct</a>
+        </div>
     </div>
+    <div className={styles.textCenter}>
+  <h2>
+  Keynote
+  </h2>
+  <ul className={styles.intro}>
+    <li className={styles.intro}>Linda Liukas</li>
+    <li className={styles.intro}>Yoko Harada</li>
+  </ul>
+</div>
+<div className={styles.textCenter}>
+  <h2>
+  Special
+  </h2>
+  <p className={styles.intro}>
+    Coming Soon
+  </p>
+  <p className={styles.specialDesc}>
+  2022年はRails Girls Japan設立10周年の年！<br />
+  お祝いのメッセージを募集しています。<br />
+  #rgj10th（仮）をつけてTweetしてくださいね！<br />
+  </p>
+</div>
+  </div>
   </Layout>
 )
 
